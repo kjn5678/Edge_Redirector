a132fc3d87704682252f53a2ff701dd8876b79c5
John Git <25sculliot@student.hpts.us>
2024-07-14 00:48:30 -0400

    I HATE Edging + find source search
    when search in the desktop searchbar, microsoft will search using the prefix "https://www.bing.com/search?q=", no matter the default browser. the extension is set to simplify the url to find the source search and report that.

--- popup/popup.js
+++ popup/popup.js
@@ -1,14 +1,29 @@
 var poop= document.getElementById("poop");
-if (poop) {
-  poop.onclick = function() {
+if (poop){
+  poop.onclick = function(){
     // do something
     const element = document.createElement('p');
     element.id = `url`;
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
       var tab = tabs[0].url;
-      console.log(tab)
+      if(tab.substring(0,30)=="https://www.bing.com/search?q="){
+        tab =filter(tab);
+      }
       element.innerText = tab;
       document.body.appendChild(element);
+      if(tab.substring(0,30)=="https://www.bing.com/search?q="){
+        element.innerText = filter(tab);
+      }
     });
   };
+}
+function filter(taburl){
+  var ss=30;
+  while(taburl[ss]!="&"){
+    ss++;
+    if (ss>taburl.length){
+      return taburl.substring(30,taburl.length);
+    }
+  }
+  return taburl.substring(30,ss);
 }
